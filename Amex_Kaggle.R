ab98ebab7e6bdb73ddc3cb71e7db6058c8569a99
Mike Gredlics <mgredlics@gmail.com>
2023-02-04 09:23:14 -0700

    Add R code

---
+++ Amex_Kaggle.R
@@ -0,0 +1,19 @@
+library(arrow)
+library(dplyr)
+library(caret)
+
+
+df <- read_parquet("~/Documents/Amex_Kaggle/train.parquet",as_tibble = TRUE)
+
+
+## 20% of the sample size
+smp_size <- floor(0.80 * nrow(df))
+
+## set the seed to make your partition reproducible
+set.seed(42)
+partition <- sample(seq_len(nrow(df)), size = smp_size)
+
+Train <- df[partition, ]
+Test <- df[-partition, ]
+
+sample <- Train[1:20,]
